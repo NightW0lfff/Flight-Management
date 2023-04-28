594dd48c73732a75ba54c00f93b5bd9cbd420d0e
NightW0lfff <philong170101@gmail.com>
2023-04-28 18:40:25 +1000

    Update passenger.cpp

--- Final Code/passenger.cpp
+++ Final Code/passenger.cpp
@@ -14,9 +14,9 @@ int main()
     flights.push_back(new InternationalFlight("F2", "New York", "London", "19:00", "07:00"));
 
     // Create passengers
-    Passenger passenger1("John Doe", 35, "1234567890");
-    Passenger passenger2("Jane Doe", 29, "0987654321");
-    Passenger passenger3("John Smith", 45, "1234567890");
+    Passenger passenger1("John Doe", 35, "1234567890", "c123123");
+    Passenger passenger2("Jane Doe", 29, "0987654321", "c321321");
+    Passenger passenger3("John Smith", 45, "1234567890", "c123123");
 
     // Bookings
     std::vector<Booking *> bookings;
@@ -26,8 +26,16 @@ int main()
     BookingSystem *bookingSys = new BookingSystem(flights, bookings);
 
     // bookingSys->displayAvailableFlights();
+    // bookingSys->displayBookingDetails();
+    // bookingSys->displayFlightDetails();
+
+    bookingSys->createBooking();
+    // bookingSys->displayBookingDetails();
+    // bookingSys->cancelBooking();
+
+    bookingSys->updateBooking();
+
     bookingSys->displayBookingDetails();
-    bookingSys->displayFlightDetails();
 
     // bookings[0]->createBooking(flights[0], passenger1);
     // bookings[0]->createBooking(flights[0], passenger2);
